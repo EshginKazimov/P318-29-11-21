2a9863b5ab5dff1c0fc97a93a2f296c5bdf64338
EshginKazimov <82080797+EshginKazimov@users.noreply.github.com>
2021-11-30 00:56:01 +0400

    Add files via upload

---
+++ FrontToBack/FrontToBack/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using FrontToBack.DataAccessLayer;
+using FrontToBack.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBack.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+
+        public HomeController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var sliderImages = _dbContext.SliderImages.ToList();
+            //var slider = _dbContext.Sliders.FirstOrDefault();
+            var slider = _dbContext.Sliders.SingleOrDefault();
+            var categories = _dbContext.Categories.ToList();
+            //var products = _dbContext.Products.Include(x => x.Category).OrderBy(x => x.Price).ToList();
+            //var products = _dbContext.Products.Include(x => x.Category).Where(x => x.Price > 200).ToList();
+            var products = _dbContext.Products.Include(x => x.Category).OrderByDescending(x => x.Price).ToList();
+            //var products = _dbContext.Products.Where(x => x.Price > 200).ToList();
+
+            return View(new HomeViewModel 
+            {
+                Slider = slider,
+                SliderImages = sliderImages,
+                Categories = categories,
+                Products = products
+            });
+        }
+    }
+}
